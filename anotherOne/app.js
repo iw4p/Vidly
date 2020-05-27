b091a84952a9a1c62cd22f12c9bc265afa5f135c
Nima Akbarzade <Nimakbarzade@yahoo.com>
2020-05-27 19:38:56 +0430

    add db

--- anotherOne/app.js
+++ anotherOne/app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+require('dotenv/config')
 
+//import routes
+
+const postsRoute = require('./routes/posts')
+
+app.use('/posts', postsRoute)
+
+//Routes
 app.get('/', (req, res) => {
     res.send('We are on home')
 });
 
-app.get('/posts', (req, res) => {
-    res.send('We are on posts')
-});
+
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, 
+{ useUnifiedTopology: true },
+() => console.log('connected to db')
+);
 
 //Listening
 app.listen(3000);
